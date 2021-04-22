73858c9ff708e588db22d7e9da4680f7887f05b5
RinaOcean <antyfifa@gmail.com>
2021-04-23 00:37:12 +0300

    выносит форму в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
 import React, { Component } from 'react';
 import ContactsList from './components/ContactsList';
 import ContactsListItem from './components/ContactListItem';
+import ContactForm from './components/ContactForm';
 
 import './App.scss';
 
 class App extends Component {
   state = {
     contacts: [],
-    name: '',
-    number: '',
+    // name: '',
+    // number: '',
   };
 
-  handleChange = event => {
-    event.preventDefault();
-    const { name, value } = event.currentTarget;
-    // console.log(event.currentTarget.name);
-    this.setState({ [name]: value });
-  };
+  // handleChange = event => {
+  //   event.preventDefault();
+  //   const { name, value } = event.currentTarget;
+  //   // console.log(event.currentTarget.name);
+  //   this.setState({ [name]: value });
+  // };
+
+  // handleSubmit = event => {
+  //   event.preventDefault();
+  //   console.log(this.state);
+  // };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts } = this.state;
     return (
       <>
         <h1>Phonebook</h1>
-        <form>
-          <label>
-            Name
-            <input
-              type="text"
-              value={name}
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Number
-            <input
-              type="tel"
-              name="number"
-              pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
-              title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-        </form>
+        <ContactForm />
         <h2>Contacts</h2>
         <ContactsList>
           <ContactsListItem contacts={contacts} />
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    event.preventDefault();
+    const { name, value } = event.currentTarget;
+    // console.log(event.currentTarget.name);
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+            title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+export default ContactForm;
