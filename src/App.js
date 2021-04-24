a728da5a9707d2b0c3edca9160155ebf6696a328
RinaOcean <antyfifa@gmail.com>
2021-04-25 01:48:28 +0300

    добавляет проверку по существующему контакту

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import './App.scss';
 
 class App extends Component {
   state = {
-    contacts: [],
-    // contacts: [
-    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    // ],
+    // contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
   formSubmitHandler = data => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data],
-    }));
+    const isInContacts = contact => {
+      return contact.name === data.name;
+    };
+
+    if (this.state.contacts.some(isInContacts)) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    } else
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, data],
+      }));
   };
 
   filterHandler = event => {
